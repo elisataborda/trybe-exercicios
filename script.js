e9a0b5c9c5c24b854077f1aaf9b3f25291b4523e
Elisa Taborda <elisamariataborda@gmail.com>
2021-11-29 19:54:44 -0300

    Exercicio 4 do bloco 4.2

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let soma = 0;
 for (let i = 0; i < numbers.length; i += 1) {
     soma += (numbers[i]);
 }
-console.log(soma/numbers.length);
+let valorFinal = soma/numbers.length;
+console.log(valorFinal);
+
+if (valorFinal > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
+
 
 
