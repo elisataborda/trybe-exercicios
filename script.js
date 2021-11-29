0a32605b0d7e52d6c9d0d5cc5adec963a16782ea
Elisa Taborda <elisamariataborda@gmail.com>
2021-11-29 19:46:45 -0300

    Exercicio 2 do bloco 4.2

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35];
-for (let i = 0; i < numbers.length; i +=1) {
-    console.log(numbers[i]);
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    soma += (numbers[i]);
 }
+console.log(soma);
+
+
